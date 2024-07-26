996062c1278d786d0f227a8367846511249a5285
Michael Florya <mih2303@mail.ru>
2024-07-26 19:23:39 +0300

    create animation for sprite

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,11 +4,13 @@ extends CharacterBody2D
 const SPEED = 100
 const JUMP_VELOCITY = -300
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta):
 	move()
 	jump()
 	apply_gravity(delta)
+	animate()
 
 func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
@@ -26,3 +28,17 @@ func jump():
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+func animate():
+	if velocity.y != 0:
+		sprite.play("jump")
+	elif velocity.x != 0:
+		sprite.play("walk")
+	else:
+		sprite.play("idle")
+		
+	if velocity.x < 0:
+		sprite.flip_h = true
+	elif velocity.x > 0:
+		sprite.flip_h = false
+		
