f33dce88e3392da6b8ecbf567b9ac4e09c54e385
Michael Florya <mih2303@mail.ru>
2024-07-26 21:02:03 +0300

    config buttons of restart & exit

---
+++ Scenes/Levels/game_o_ver.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_restart_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
