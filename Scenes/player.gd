a51fce07e938afaea3f3c0567e083bfbc3bb1100
Michael Florya <mih2303@mail.ru>
2024-07-26 16:30:05 +0300

    create gravity for sprite

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 100
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		velocity.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
