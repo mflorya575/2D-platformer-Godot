474f50a4e7259791d58d6a51e3446fe45cd82bc5
Michael Florya <mih2303@mail.ru>
2024-07-26 16:35:22 +0300

    create system of jumping

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 100
+const JUMP_VELOCITY = -300
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		velocity.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
