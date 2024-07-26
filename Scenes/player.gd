eb934e56f6bc485caf68af50fe9c189ec1b27cd5
Michael Florya <mih2303@mail.ru>
2024-07-26 16:39:30 +0300

    refactor of code

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,10 +6,11 @@ const JUMP_VELOCITY = -300
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	move()
+	jump()
+	apply_gravity(delta)
+
+func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		velocity.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
@@ -17,3 +18,11 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 	move_and_slide()
+	
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
